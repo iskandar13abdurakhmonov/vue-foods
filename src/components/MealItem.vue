5be421ca3445250bddad547af7ac44d2f28a2f98
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2024-07-01 09:15:39 +0500

    Iskandar

--- src/components/MealItem.vue
+++ src/components/MealItem.vue
@@ -16,7 +16,7 @@ const { meal } = defineProps({
     <div class="p-3">
       <h3 class=" py-2 font-semibold">{{ meal.strMeal }}</h3>
       <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet commodi consequatur culpa cumque
-        doloribus et
+        doloribus et Iskandar
         id, illum iure libero minus, molestiae nulla odio omnis provident quam quidem quos reiciendis vitae!</p>
       <div class="flex items-center justify-between">
         <YouTubeButton :href="meal.strYoutube"/>
